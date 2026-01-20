54dcdf61d7c756a29234e6b06e8cceba57837fac
mohamadalabsi <absimoda1@gmail.com>
2026-01-20 17:58:51 +0100

    open-api-docs

--- src/main/java/com/pm/patientservice/controller/PatientController.java
+++ src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -4,23 +4,23 @@ package com.pm.patientservice.controller;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
-import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.service.PatientService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.groups.Default;
-import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import tools.jackson.databind.ser.jdk.JDKKeySerializers;
+
 
 import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("/api/v1")
 @RestController
+@Tag(name="Patient", description = "Patient management APIs")
 public class PatientController {
 
     private PatientService patientService;
@@ -36,12 +36,16 @@ public class PatientController {
     }
 
     @GetMapping("/patients")
+    @Operation(summary = "Get Patients Version 1.0")
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
 //        return new ResponseEntity<>(patientService.getAllPatients() , HttpStatus.OK) ;
         return ResponseEntity.ok().body(patientService.getAllPatients());
     }
 
-    @PostMapping("/patients")                         // this  annotation is used to reform the
+    @PostMapping("/patients")
+    @Operation(summary = "Create new Patient Version 1.0")
+
+    // this  annotation is used to reform the
     // validation on our PatientRequestDTO to make sure all the properties match the validation annotations
     // rules we added there in the PatientRequestDTO object (valid annotation)
 //    but validated annotation works as same as valid annotation but is used when we want to
@@ -53,6 +57,7 @@ public class PatientController {
 //    delete and put
 
     @PutMapping("/patients/{id}")
+    @Operation(summary = "Update Patient Version 1.0")
     public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id,
                                                             @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
 //        we are using the same dto so the validation is triggered again here for all fields, we do
@@ -62,9 +67,15 @@ public class PatientController {
     }
 
     @DeleteMapping("/patients/{id}")
+    @Operation(summary = "Delete Patient Version 1.0")
     public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
         patientService.deletePatient(id);
         return ResponseEntity.noContent().build();
     }
+
+//    now all these new annotations are for swagger documentation to generate documentation for
+//    our endpoints to describes what our endpoints (API) do
+//    this allows developers and if third party want to use our api they can see the docs
+//     so open api and swagger is  a tool and standard for designing and interacting with api
 
 }
